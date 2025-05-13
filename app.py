e7508a7090f55917827fff42905436ebd76f94ec
kimchi0812 <kimchi_ramen2003@yahoo.com>
2025-05-13 13:21:44 +0000

    3

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 #gemini
-
 from flask import Flask,request,render_template
-
 app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
+@app.route("/gemini",methods=["GET","POST"])
+def gemini():
+    return(render_template("gemini.html"))
 if __name__ == "__main__":
     app.run()
