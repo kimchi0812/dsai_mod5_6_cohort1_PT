14d0ee17db5192dc88f51d93cd3f3be9d562937a
kimchi0812 <kimchi_ramen2003@yahoo.com>
2025-05-26 14:21:19 +0000

    8

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ genmini_model = "gemini-2.0-flash"
 genai1.configure(api_key=gemini_api_key)
 model = genai1.GenerativeModel("gemini-2.0-flash")
 
-gemini_telegram_token = os.getenv('telegram_api_key')
+telegram_api_key = os.getenv('telegram_api_key')
 
 app = Flask(__name__)
 
